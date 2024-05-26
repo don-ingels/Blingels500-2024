d7317913746bfa5ea3b8900b306b0e9f75f40615
don-ingels <don@ingels.cloud>
2024-05-25 19:37:30 -0700

    Added initial Visual Studio solution

---
+++ Shared.DTO/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shared.DTO
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ WebAssembly.DataAccess/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebAssembly.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
